5a21501616d4666b7676aa6a472c0c36356f1e69
Noel R Loera J <91570298+jaramillo041104@users.noreply.github.com>
2021-11-03 17:02:09 -0600

    Add files via upload

---
+++ empleados.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>20 Noel Loera </title>
+    
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<img src="img/logo.png">
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="empleados.php">Empleado</a></li>
+					<li><a href="acta.php">Acta de nacimiento </a></li>
+					
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Bienvenidos al Registro Civil</h1>
+			</div>
+
+			<div class="articulo">
+				<article>
+					<form action="formpost.php" method="post">
+						Nombre del empleado : <input type="text" name="nombre"><br><br>
+						Fecha de nacimiento : <input type="text" name="email"><br><br>
+						Direccion : <input type="text" name="email"><br><br>
+						<input type="submit" value="Enviar">
+					</form>
+				</article>
+			</div>
+
+			<aside>
+				<div class="widget">
+					<h3>Tablas del Registro Civil</h3>
+					<ul>
+						<li><a href="empleados.php"> Empleado</a></li>
+						<li><a href="acta.php">Acta de nacimiento </a></li>
+
+					</ul>
+				</div>
+			</aside>
+		</div>
+	</section>
+	<footer>
+		<div class="wrapp">
+            <p> 20 Noel Loera </p>
+            <p> // SUBMÓDULO 2
+                Desarrolla aplicaciones Web con conexión a bases de datos</p>
+            <p> https://github.com/jaramillo041104 </p>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>20 Noel Loera </title>
+    
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<img src="img/logo.png"> 
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="empleados.php">Empleado</a></li>
+					<li><a href="acta.php">Acta de nacimiento </a></li>
+				
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Bienvenidos al Registro Civil</h1>
+			</div>
+
+			<div class="articulo">
+				<article>
+					<p>El Registro Civil tiene por objeto hacer constar los hechos y actos del estado civil de las personas.</p>
+					<br/>
+					<p> Los titulares de las Representaciones de México en el Exterior, quienes actúan en su calidad de Oficial del Registro Civil, tienen la facultad de levantar actas de nacimiento, matrimonio y defunción, así como de expedir sus copias certificadas. </p>
+					<br/>
+					<p>Los actos del registro civil efectuados en las Oficinas Consulares, tales como actas  de nacimiento, matrimonio y defunción y sus copias certificadas, surten efecto jurídico pleno en territorio nacional sin necesidad de legalización o apostilla, ni de inscripción alguna ante autoridad en México, por tratarse de documentos expedidos por una autoridad federal mexicana.</p>
+					<br/>
+					<p> La Ley de Nacionalidad que entró en vigor el 20 de marzo de 1998, estableció que las personas que nazcan en el extranjero, hijos de padres mexicanos nacidos en territorio nacional o de padre o madre mexicanos nacidos en México, son mexicanos por nacimiento siempre y cuando hayan nacido dentro de los 300 días posteriores al 20 de marzo de 1998, fecha en que entró en vigor la reforma constitucional de no pérdida de nacionalidad mexicana.</p>
+				</article>
+			</div>
+
+			<aside>
+				<div class="widget">
+					<h3>Tablas del Registro Civil</h3>
+					<ul>
+						<li><a href="empleados.php"> Empleado</a></li>
+						<li><a href="acta.php">Acta de Nacimiento</a></li>
+						
+					</ul>
+				</div>
+			</aside>
+		</div>
+	</section>
+	<footer>
+		<div class="wrapp">
+            <p> 20 Noel Loera  </p>
+            <p> // SUBMÓDULO 2
+                Desarrolla aplicaciones Web con conexión a bases de datos</p>
+            <p>https://github.com/jaramillo041104 </p>
+		</div>
+	</footer>
+</body>
+</html>
